0f0618a2b44f1f43d9b119b6b43b99ee93fa8aee
gaohei <admin@cxcp.com>
2021-12-09 01:56:29 +0000

    update server/src/main/java/cn/keking/service/impl/PdfYashuo.java.

--- server/src/main/java/cn/keking/service/impl/PdfYashuo.java
+++ server/src/main/java/cn/keking/service/impl/PdfYashuo.java
@@ -11,7 +11,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class Pdfyashuo {
+public class PdfYashuo {
 
     /**
      * 图像的乘法因子
