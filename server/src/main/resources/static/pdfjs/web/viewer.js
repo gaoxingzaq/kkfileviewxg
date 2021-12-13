3520a4e1f86b4c44b63a7130aa90917e643099ac
gaohei <admin@cxcp.com>
2021-12-13 13:32:06 +0000

    update server/src/main/resources/static/pdfjs/web/viewer.js. 限制右键

--- server/src/main/resources/static/pdfjs/web/viewer.js
+++ server/src/main/resources/static/pdfjs/web/viewer.js
@@ -2847,10 +2847,12 @@ function webViewerInitialized() {
   var appConfig = PDFViewerApplication.appConfig;
   var file;
   var disableDownload;
+  var pdfXianzhi;
   var queryString = document.location.search.substring(1);
   var params = (0, _ui_utils.parseQueryString)(queryString);
   file = "file" in params ? params.file : _app_options.AppOptions.get("defaultUrl");
    disableDownload = 'disabledownload' in params ? params.disabledownload : 'false';
+   pdfXianzhi = 'pdfXianzhi' in params ? params.pdfXianzhi : 'true';
   validateFileURL(file);
   var fileInput = document.createElement("input");
   fileInput.id = appConfig.openFileInputName;
@@ -2942,8 +2944,133 @@ function webViewerInitialized() {
    if ('true' === disableDownload) {
     document.getElementById("print").style.display='none';
   }
+    if ('true' === disableDownload) {
+    document.getElementById("viewBookmark").style.display='none';
+  }
+   if ('true' === pdfXianzhi) {
+  //屏蔽右键菜单
+document.oncontextmenu = function (event) {
+  if (window.event) {
+    event = window.event;
+  }
+  try {
+    var the = event.srcElement;
+    if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) {
+      return false;
+    }
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+//屏蔽粘贴
+document.onpaste = function (event) {
+  if (window.event) {
+    event = window.event;
+  }
+  try {
+    var the = event.srcElement;
+    if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) {
+      return false;
+    }
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+//屏蔽复制
+
+//屏蔽剪切
+document.oncut = function (event) {
+  if (window.event) {
+    event = window.event;
+  }
+  try {
+    var the = event.srcElement;
+    if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) {
+      return false;
+    }
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+//禁止f12
+
+function fuckyou() {
+  window.close(); //关闭当前窗口(防抽)
+  window.location = "about:blank"; //将当前窗口跳转置空白页
+}
+//禁止Ctrl+U
+var arr = [123, 17, 18];
+document.oncontextmenu = new Function("event.returnValue=false;"), //禁用右键
+
+  window.onkeydown = function (e) {
+    var keyCode = e.keyCode || e.which || e.charCode;
+    var ctrlKey = e.ctrlKey || e.metaKey;
+    console.log(keyCode + "--" + keyCode);
+    if (ctrlKey && keyCode == 85) {
+      e.preventDefault();
+    }
+    if (arr.indexOf(keyCode) > -1) {
+      e.preventDefault();
+    }
+  }
+
+function ck() {
+  console.profile();
+  console.profileEnd();
+  //我们判断一下profiles里面有没有东西，如果有，肯定有人按F12了，没错！！
+  if (console.clear) {
+    console.clear()
+  };
+  if (typeof console.profiles == "object") {
+    return console.profiles.length > 0;
+  }
 }
 
+function hehe() {
+  if ((window.console && (console.firebug || console.table && /firebug/i.test(console.table()))) || (
+      typeof opera ==
+      'object' && typeof opera.postError == 'function' && console.profile.length > 0)) {
+    fuckyou();
+  }
+  if (typeof console.profiles == "object" && console.profiles.length > 0) {
+    fuckyou();
+  }
+}
+hehe();
+window.onresize = function () {
+  if ((window.outerHeight - window.innerHeight) > 200)
+    //判断当前窗口内页高度和窗口高度，如果差值大于200，那么呵呵
+    fuckyou();
+}
+
+document.onkeydown = function (event) {
+  if ((event.keyCode == 112) || //屏蔽 F1
+    (event.keyCode == 113) || //屏蔽 F2
+    (event.keyCode == 114) || //屏蔽 F3
+    (event.keyCode == 115) || //屏蔽 F4
+    // (event.keyCode == 116) || //屏蔽 F5
+    (event.keyCode == 117) || //屏蔽 F6
+    (event.keyCode == 118) || //屏蔽 F7
+    (event.keyCode == 119) || //屏蔽 F8
+    (event.keyCode == 120) || //屏蔽 F9
+    (event.keyCode == 121) || //屏蔽 F10
+    (event.keyCode == 122) || //屏蔽 F11
+    (event.keyCode == 123)) //屏蔽 F12
+  {
+    return false;
+  }
+}
+window.onhelp = function () {
+  return false;
+}
+  }
+}
+
+
+  
 function webViewerOpenFileViaURL(file) {
   if (file) {
     PDFViewerApplication.open(file);
