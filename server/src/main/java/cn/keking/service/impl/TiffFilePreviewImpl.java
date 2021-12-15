6e9b3609037521ac5faea733eefdd36a82b4f77f
gaoxiongzaq <admin@cxcp.com>
2021-12-15 11:18:32 +0800

    重构 tif文件 多图转换模式

--- server/src/main/java/cn/keking/service/impl/TiffFilePreviewImpl.java
+++ server/src/main/java/cn/keking/service/impl/TiffFilePreviewImpl.java
@@ -2,19 +2,19 @@ package cn.keking.service.impl;
 
 import cn.keking.config.ConfigConstants;
 import cn.keking.model.FileAttribute;
-import cn.keking.model.FileType;
 import cn.keking.model.ReturnResponse;
+import cn.keking.service.FileHandlerService;
 import cn.keking.service.FilePreview;
 import cn.keking.utils.ConvertPicUtil;
 import cn.keking.utils.DownloadUtils;
 import cn.keking.utils.WebUtils;
 import cn.keking.web.filter.BaseUrlFilter;
+import jodd.util.StringUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,30 +25,36 @@ import java.util.UUID;
  */
 @Service
 public class TiffFilePreviewImpl implements FilePreview {
+    private final FileHandlerService fileHandlerService;
+    private final OtherFilePreviewImpl otherFilePreview;
+    private static final String FILE_DIR = ConfigConstants.getFileDir();
 
-    private final PictureFilePreviewImpl pictureFilePreview;
+    public TiffFilePreviewImpl(FileHandlerService fileHandlerService, OtherFilePreviewImpl otherFilePreview) {
+        this.fileHandlerService = fileHandlerService;
+        this.otherFilePreview = otherFilePreview;
+    }
     private static final String INITIALIZE_MEMORY_SIZE = "initializeMemorySize";
     //默认初始化 50MB 内存
     private static final long INITIALIZE_MEMORY_SIZE_VALUE_DEFAULT = 1024L * 1024 * 50;
-
-    private final String fileDir = ConfigConstants.getFileDir();
-
-    public TiffFilePreviewImpl(PictureFilePreviewImpl pictureFilePreview) {
-        this.pictureFilePreview = pictureFilePreview;
-    }
-
     @Override
     public String filePreviewHandle(String url, Model model, FileAttribute fileAttribute) {
-
-        String tifPreviewType = ConfigConstants.getTifPreviewType();
-        String tifOnLinePreviewType = fileAttribute.getTifPreviewType();
-        if (StringUtils.hasText(tifOnLinePreviewType)) {
-            tifPreviewType = tifOnLinePreviewType;
+        String baseUrl = BaseUrlFilter.getBaseUrl();
+        String gengxin=fileAttribute.getgengxin();
+        String fileName = fileAttribute.getName();
+        String officePreviewType = fileAttribute.getOfficePreviewType();
+        String pdfName = fileName.substring(0, fileName.lastIndexOf(".") + 1) + "pdf";
+        String outFilePath = FILE_DIR + pdfName;
+        String tifoutFilePath = FILE_DIR + fileName;
+        String  host = FileHandlerService.hqurl(url);
+        boolean bendi = FileHandlerService.kuayu(host, baseUrl); //判断是否是本地URL 是本地的启用分页功能 不是就直接在跨域输出
+        boolean pdfgx ;
+        if(StringUtil.isNotBlank(gengxin) && "ok".equalsIgnoreCase(gengxin)) { //去缓存更新
+            pdfgx= false;
+        }else {
+            pdfgx= ConfigConstants.isCacheEnabled();
         }
-
+        String tifPreviewType = ConfigConstants.getTifPreviewType();
         if("tif".equalsIgnoreCase(tifPreviewType)){
-
-            pictureFilePreview.filePreviewHandle(url,model,fileAttribute);
             String fileSize = WebUtils.getUrlParameterReg(url,INITIALIZE_MEMORY_SIZE);
             if(StringUtils.hasText(fileSize)){
                 model.addAttribute(INITIALIZE_MEMORY_SIZE,fileSize);
@@ -56,58 +62,45 @@ public class TiffFilePreviewImpl implements FilePreview {
                 model.addAttribute(INITIALIZE_MEMORY_SIZE,Long.toString(INITIALIZE_MEMORY_SIZE_VALUE_DEFAULT));
             }
             return TIFF_FILE_PREVIEW_PAGE;
-
-        }else if("jpg".equalsIgnoreCase(tifPreviewType) || "pdf".equalsIgnoreCase(tifPreviewType)){
-            String inputFileName = url.substring(url.lastIndexOf("/") + 1);
-            String inputFileExt = inputFileName.substring(inputFileName.lastIndexOf(".") + 1);
-            String uuid = UUID.randomUUID().toString().replaceAll("-","");
-            String tiffFileName = uuid + "." + inputFileExt;
-
-            ReturnResponse<String> response = DownloadUtils.downLoad(fileAttribute, tiffFileName);
-            if (response.isFailure()) {
-                return NOT_SUPPORTED_FILE_PAGE;
-            }
-            String strTiffPath = response.getContent();
-
-            File fileTiff = new File(strTiffPath);
-
-            File fileJpg = ConvertPicUtil.convertPic2Jpg(strTiffPath, fileDir + uuid + ".jpg");
-
-            if(fileJpg.exists()){
-                // 转换后的tif没用了，可以删掉了
-                if(fileTiff.exists()){
-                    fileTiff.delete();
-                }
-
-                String baseUrl = BaseUrlFilter.getBaseUrl();
-                if("pdf".equalsIgnoreCase(tifPreviewType)){
-                    File filePdf = ConvertPicUtil.convertJpg2Pdf(fileDir + uuid + ".jpg", fileDir + uuid + ".pdf");
-                    if(filePdf.exists()){
-                        String pdfUrl = baseUrl + uuid + ".pdf";
-                        model.addAttribute("pdfUrl", pdfUrl);
-
-                        return PDF_FILE_PREVIEW_PAGE;
+        }else if("pdf".equalsIgnoreCase(tifPreviewType)){
+            if (!pdfgx ||!fileHandlerService.listConvertedFiles().containsKey(pdfName) || !ConfigConstants.isCacheEnabled()) {
+                if(!bendi){
+                    ReturnResponse<String> response = DownloadUtils.downLoad(fileAttribute, fileName);
+                    if (response.isFailure()) {
+                        return otherFilePreview.notSupportedFile(model, fileAttribute, response.getMsg());
                     }
-                }else{
-                    String jpgUrl = baseUrl + uuid + ".jpg";
-
-                    fileAttribute.setName(uuid + ".jpg");
-                    fileAttribute.setType(FileType.PICTURE);
-                    fileAttribute.setSuffix("jpg");
-                    fileAttribute.setUrl(jpgUrl);
-
-                    List<String> imgUrls = new ArrayList<>();
-                    imgUrls.add(jpgUrl);
-
-                    model.addAttribute("imgUrls", imgUrls);
-                    model.addAttribute("currentUrl", jpgUrl);
+                    tifoutFilePath = response.getContent();
+                    ConvertPicUtil.convertJpg2Pdf(tifoutFilePath, outFilePath);
+                }else {
+                    File file = new File(tifoutFilePath);   //判断文件是否存在
+                    if(!file.exists() || file.length() == 0) {
+                        tifoutFilePath = FILE_DIR +"demo/" + fileName;
+                    }
+                    File filee = new File(tifoutFilePath);   //判断文件是否存在
+                    if(!filee.exists() || filee.length() == 0) {
+                        return otherFilePreview.notSupportedFile(model, fileAttribute, "文件不存在");
+                    }
+                    ConvertPicUtil.convertJpg2Pdf(tifoutFilePath, outFilePath);
                 }
-
+              //  System.out.println(tifoutFilePath);
+            if (ConfigConstants.isCacheEnabled()) {
+                // 加入缓存
+                fileHandlerService.addConvertedFile(pdfName, fileHandlerService.getRelativePath(outFilePath));
             }
-
-            return PICTURE_FILE_PREVIEW_PAGE;
+            }
+            if ("pdf".equalsIgnoreCase(officePreviewType)) {
+                model.addAttribute("pdfUrl", pdfName);
+                return PDF_FILE_PREVIEW_PAGE;
+            } else {
+                List<String> imageUrls = fileHandlerService.pdf2jpg(outFilePath, pdfName, baseUrl,fileAttribute);
+                if (imageUrls == null || imageUrls.size() < 1) {
+                    return otherFilePreview.notSupportedFile(model, fileAttribute, "pdf转图片异常，请联系管理员");
+                }
+                model.addAttribute("imgurls", imageUrls);
+                model.addAttribute("currentUrl", imageUrls.get(0));
+                return OFFICE_PICTURE_FILE_PREVIEW_PAGE;
+            }
         }
-
         return NOT_SUPPORTED_FILE_PAGE;
     }
 }
--- server/src/main/java/cn/keking/utils/ConvertPicUtil.java
+++ server/src/main/java/cn/keking/utils/ConvertPicUtil.java
@@ -1,57 +1,17 @@
 package cn.keking.utils;
-
-
-import com.lowagie.text.Document;
-import com.lowagie.text.Image;
-import com.lowagie.text.Rectangle;
-import com.lowagie.text.pdf.PdfWriter;
-import com.sun.media.jai.codec.ImageCodec;
-import com.sun.media.jai.codec.ImageEncoder;
-import com.sun.media.jai.codec.JPEGEncodeParam;
-
-import javax.media.jai.JAI;
-import javax.media.jai.RenderedOp;
-import java.io.*;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.io.FileChannelRandomAccessSource;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.RandomAccessFileOrArray;
+import com.itextpdf.text.pdf.codec.TiffImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
 
 public class ConvertPicUtil {
-
-    /**
-     *  图片 转  JPG。
-     *  支持输入格式如下：BMP、GIF、FlashPix、JPEG、PNG、PMN、TIFF、WBMP
-     * @param strInputFile 输入文件的路径和文件名
-     * @param strOutputFile 输出文件的路径和文件名
-     * @return
-     */
-    public static File convertPic2Jpg(String strInputFile, String strOutputFile) {
-        // 读取源图片文件
-        RenderedOp roInput = JAI.create("fileload", strInputFile);
-        try {
-            strOutputFile = strOutputFile.replaceAll("\\\\", "/");
-            File fileJpgPath = new File(strOutputFile.substring(0, strOutputFile.lastIndexOf("/")));
-            if(!fileJpgPath.exists()){
-                fileJpgPath.mkdirs();
-            }
-
-            File fileJpg=new File(strOutputFile);
-            OutputStream ops = new FileOutputStream(fileJpg);
-            // 文件存储输出流
-            JPEGEncodeParam param = new JPEGEncodeParam();
-            ImageEncoder image = ImageCodec.createImageEncoder("JPEG", ops,
-                    param); // 指定输出格式
-            // 解析输出流进行输出
-            image.encode(roInput);
-            // 关闭流
-            ops.close();
-
-            return fileJpg;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     /**
      * 将Jpg图片转换为Pdf文件
      * @param strJpgFile 输入的jpg的路径和文件名
@@ -59,47 +19,34 @@ public class ConvertPicUtil {
      * @return
      */
     public static File convertJpg2Pdf(String strJpgFile, String strPdfFile) {
-        Document document = new Document();
-        // 设置文档页边距
-        document.setMargins(0,0,0,0);
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(strPdfFile);
-            PdfWriter.getInstance(document, fos);
-            // 打开文档
+        try
+        {
+            RandomAccessFile aFile = new RandomAccessFile(strJpgFile, "r");
+            FileChannel inChannel = aFile.getChannel();
+            FileChannelRandomAccessSource fcra =  new FileChannelRandomAccessSource(inChannel);
+            Document document = new Document();
+            PdfWriter.getInstance(document,  new FileOutputStream(strPdfFile));
             document.open();
-            // 获取图片的宽高
-            Image image = Image.getInstance(strJpgFile);
-            float floatImageHeight=image.getScaledHeight();
-            float floatImageWidth=image.getScaledWidth();
-            // 设置页面宽高与图片一致
-            Rectangle rectangle = new Rectangle(floatImageWidth, floatImageHeight);
-            document.setPageSize(rectangle);
-            // 图片居中
-            image.setAlignment(Image.ALIGN_CENTER);
-            //新建一页添加图片
-            document.newPage();
-            document.add(image);
-        } catch (Exception ioe) {
-            ioe.printStackTrace();
-            return null;
-        } finally {
-            //关闭文档
-            document.close();
-            try {
-                fos.flush();
-                fos.close();
-
-                File filePDF = new File(strPdfFile);
-                return filePDF;
-            } catch (IOException e) {
-                e.printStackTrace();
+            RandomAccessFileOrArray rafa = new RandomAccessFileOrArray(fcra);
+            int pages = TiffImage.getNumberOfPages(rafa);
+            Image image;
+            for (int i = 1; i <= pages; i++) {
+                image = TiffImage.getTiffImage(rafa, i);
+                Rectangle pageSize = new Rectangle(image.getWidth(), image.getHeight());
+                document.setPageSize(pageSize);
+                document.newPage();
+                document.add(image);
             }
-
+            document.close();
+            aFile.close();
+            File filePDF = new File(strPdfFile);
+            return filePDF;
         }
-
+        catch (Exception e)
+        {
+            System.out.println(e.toString());
+        }
         return null;
     }
-
 }
 
