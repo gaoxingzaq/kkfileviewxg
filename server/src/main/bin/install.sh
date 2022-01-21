9cbcc0d5926f25e5e42bfef182e61a44cd1662ed
gaoxiongzaq <admin@cxcp.com>
2022-01-21 11:52:08 +0800

    更新 Luckysheet

--- server/src/main/bin/install.sh
+++ server/src/main/bin/install.sh
@@ -7,7 +7,6 @@ install_redhat() {
    if [ $? -eq 0 ];then
      yum install -y libXext.x86_64
      yum groupinstall -y  "X Window System"
-     yum installlocalho
      yum localinstall *.rpm
      echo 'install finshed...'
    else
@@ -16,7 +15,7 @@ install_redhat() {
 }
 
 install_ubuntu() {
-   wget  https://kkfileview.keking.cn/LibreOffice_7.1.4_Linux_x86-64_deb.tar.gz  -cO LibreOffice_7_deb.gz && tar -zxf /tmp/LibreOffice_7_deb.tar.gz && cd /tmp/LibreOffice_7.1.4.2_Linux_x86-deb/RPMS
+   wget  https://kkfileview.keking.cn/LibreOffice_7.1.4_Linux_x86-64_deb.tar.gz  -cO LibreOffice_7_deb.tar.gz && tar -zxf /tmp/LibreOffice_7_deb.tar.gz && cd /tmp/LibreOffice_7.1.4.2_Linux_x86-64_deb/DEBS
    echo $?
  if [ $? -eq 0 ];then
      apt-get install -y libxinerama1 libcairo2 libcups2 libx11-xcb1
