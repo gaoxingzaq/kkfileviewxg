013110e28c5eae938b2f88b43b1cc39acbf4fe74
gaoxiongzaq <admin@cxcp.com>
2022-02-12 15:18:32 +0800

    更新LibreOffice到7.3

---
+++ server/src/main/bin/LibreOffice7.3.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cd /tmp
+install_redhat() {
+    wget https://mirrors.cloud.tencent.com/libreoffice/libreoffice/stable/7.3.0/rpm/x86_64/LibreOffice_7.3.0_Linux_x86-64_rpm.tar.gz -cO libreoffice_rpm.tar.gz && tar zxf /tmp/libreoffice_rpm.tar.gz && cd /tmp/LibreOffice_7.3.0.3_Linux_x86-64_rpm/RPMS
+   echo $?
+   if [ $? -eq 0 ];then
+     yum install -y libXext.x86_64
+     yum groupinstall -y  "X Window System"
+     yum installlocalho
+     yum localinstall *.rpm
+     echo 'install finshed...'
+   else
+     echo 'download package error...'
+   fi
+}
+
+install_ubuntu() {
+     wget https://mirrors.cloud.tencent.com/libreoffice/libreoffice/stable/7.3.0/deb/x86_64/LibreOffice_7.3.0_Linux_x86-64_deb.tar.gz -cO libreoffice_deb.tar.gz && tar zxf /tmp/libreoffice_deb.tar.gz && cd /tmp/LibreOffice_7.3.0.3_Linux_x86-64_deb/DEBS
+   echo $?
+ if [ $? -eq 0 ];then
+     apt-get install -y libxinerama1 libcairo2 libcups2 libx11-xcb1
+     dpkg -i *.deb
+     echo 'install finshed...'
+  else
+    echo 'download package error...'
+ fi
+}
+
+
+if [ -f "/etc/redhat-release" ]; then
+  yum install -y wget
+  install_redhat
+else
+  apt-get install -y wget
+  install_ubuntu
+fi
